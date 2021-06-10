1e20d342390de6f320bdbaea63dc632994609c76
elmedinkarisik <edo.karisik1@gmail.com>
2021-06-10 21:24:08 +0200

    Install styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `visitBosnia.ba`,
+    description: `Website for promoting tourism in Bosnia and Herzegovina`,
     author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
